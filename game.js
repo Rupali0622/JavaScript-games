04d28cfe43c88635b80f99ab3a82a8b7adc7844e
Rupali <rupalichauhan022@gmail.com>
2024-01-23 20:29:35 +0530

    Tic-Tac-Toe

--- game.js
+++ game.js
@@ -1,23 +1,72 @@
-let boxes=document.querySelector(".box");
-let reset=document.querySelector("#reset-button")
+let btn = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset-button");
+let newgame=document.querySelector("#new-btn");
+let msgcont=document.querySelector(".msg-container");
+let msg=document.querySelector("p");
 
-let turn=true;
+let turn = true;
 
-const winningPattern=[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [7,6,8]
+const winningPattern = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [7, 6, 8]
 ];
+const resertGame=()=>{
+    turn=true;
+    enableBoxes();
+    msgcont.classList.add("hide");
+};
 
-boxes.forEach(element => {
-    
-});((box)=>{
-    box.addEventListener("click",()=>{
-        console.log("box was clicked");
+btn.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turn===true){
+            turn=false;
+            box.innerText="X"
+        }else{
+            turn=true;
+            box.innerText="O"
+        }
+        checkWinner();
     });
 });
+
+const disableBoxes=()=>{
+    for(let box of btn){
+        box.disabled=true;
+    }
+};
+
+const enableBoxes=()=>{
+    for(let box of btn){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulations, Winner is ${winner}`;
+    msgcont.classList.remove("hide");
+    disableBoxes();
+}
+const checkWinner=()=>{
+    for(let pattern of winningPattern){
+        let pos1val= btn[pattern[0]].innerText;
+        let pos2val= btn[pattern[1]].innerText;
+        let pos3val= btn[pattern[2]].innerText;
+
+        if(pos1val != "" && pos2val != "" && pos3val!=""){
+            if(pos1val=== pos2val && pos2val===pos3val){
+                console.log("Winner!",pos1val);
+                showWinner(pos1val);
+            }
+        }
+    }
+};
+
+newgame.addEventListener("click",resertGame);
+reset.addEventListener("click",resertGame);
